b1715639e422b65baeeceba1b5c3863580ef3eab
NeoRazorX <neorazorx@gmail.com>
2016-08-01 20:38:12 +0200

    Corregido bug al detectar la clave primaria de la tabla, impidiendo así generar el código de las funciones exists() y delete().

--- controller/fsdk_tabla.php
+++ controller/fsdk_tabla.php
@@ -13,6 +13,8 @@ class fsdk_tabla extends fs_controller
    public $xml;
    
    private $columnas;
+   private $primary_key;
+   private $restricciones;
    
    public function __construct()
    {
@@ -67,12 +69,12 @@ class fsdk_tabla extends fs_controller
       /// creamos el xml
       $archivo_xml = simplexml_load_string($cadena_xml);
       $this->columnas = Array();
-      $restricciones = Array();
+      $this->restricciones = Array();
       if( $this->db->table_exists($this->tabla) )
       {
-         $primary_key = '...';
+         $this->primary_key = '...';
          $this->columnas = $this->db->get_columns($this->tabla);
-         $restricciones = $this->db->get_constraints($this->tabla);
+         $this->restricciones = $this->db->get_constraints($this->tabla);
          
          if($this->columnas)
          {
@@ -86,7 +88,7 @@ class fsdk_tabla extends fs_controller
                {
                   if($col['key'] == 'PRI')
                   {
-                     $primary_key = $col['column_name'];
+                     $this->primary_key = $col['column_name'];
                   }
                }
                
@@ -115,7 +117,7 @@ class fsdk_tabla extends fs_controller
                }
                else if($col['data_type'] == 'integer' AND $col['column_default'] == "nextval('".$this->tabla.'_'.$col['column_name']."_seq'::regclass)") /// comprobamos si es tipo serial
                {
-                  $primary_key = $col['column_name'];
+                  $this->primary_key = $col['column_name'];
                   
                   $aux->addChild('tipo', 'serial');
                   
@@ -152,9 +154,9 @@ class fsdk_tabla extends fs_controller
             }
          }
          
-         if($restricciones)
+         if($this->restricciones)
          {
-            foreach($restricciones as $col)
+            foreach($this->restricciones as $col)
             {
                $aux = $archivo_xml->addChild('restriccion');
                
@@ -174,7 +176,7 @@ class fsdk_tabla extends fs_controller
                         break;
                         
                      case 'p':
-                        $aux->addChild('consulta', 'PRIMARY KEY ('.$primary_key.')');
+                        $aux->addChild('consulta', 'PRIMARY KEY ('.$this->primary_key.')');
                         break;
                      
                      case 'f':
@@ -190,7 +192,7 @@ class fsdk_tabla extends fs_controller
                {
                   if($col['tipo'] == 'PRIMARY KEY')
                   {
-                     $aux->addChild('consulta', 'PRIMARY KEY ('.$primary_key.')');
+                     $aux->addChild('consulta', 'PRIMARY KEY ('.$this->primary_key.')');
                   }
                   else
                      $aux->addChild('consulta', $col['tipo'].' (...)');
@@ -214,129 +216,145 @@ class fsdk_tabla extends fs_controller
       $this->modelo = "<?php\n\n"
               . "class ".$this->nombre_modelo." extends fs_model\n{\n";
       
-      foreach($this->columnas as $col)
+      if($this->columnas)
       {
-         $this->modelo .= $tab.'public $'.$col['column_name'].";\n";
+         foreach($this->columnas as $col)
+         {
+            if($col['column_name'] == $this->primary_key)
+            {
+               $this->modelo .= "\n".$tab."/// clave primaria. ".$col['data_type']."\n";
+               $this->modelo .= $tab.'public $'.$col['column_name'].";\n\n";
+            }
+            else
+            {
+               $this->modelo .= $tab.'public $'.$col['column_name'].";\n";
+            }
+         }
+         
+         $this->modelo .= "\n"
+                 . $tab.'public function __construct($d=FALSE)'."\n"
+                 . $tab."{\n"
+                 . $tab.$tab."parent::__construct('".$this->tabla."');\n"
+                 . $tab.$tab.'if($d)'."\n"
+                 . $tab.$tab."{\n";
+         
+         foreach($this->columnas as $col)
+         {
+            if($col['data_type'] == 'boolean' OR $col['data_type'] == 'tinyint(1)')
+            {
+               $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name'].' = $this->str2bool($d'."['".$col['column_name']."']);\n";
+            }
+            else if($col['data_type'] == 'date')
+            {
+               $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name'].' = date("d-m-Y", strtotime($d'."['".$col['column_name']."']));\n";
+            }
+            else if($col['data_type'] == 'double')
+            {
+               $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name'].' = floatval($d'."['".$col['column_name']."']);\n";
+            }
+            else if($col['data_type'] == 'integer')
+            {
+               $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name'].' = $this->intval($d'."['".$col['column_name']."']);\n";
+            }
+            else
+            {
+               $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name'].' = $d'."['".$col['column_name']."'];\n";
+            }
+         }
+         
+         $this->modelo .= $tab.$tab."}\n"
+                 . $tab.$tab."else\n"
+                 . $tab.$tab."{\n"
+                 . $tab.$tab.$tab."/// valores predeterminados\n";
+         
+         foreach($this->columnas as $col)
+         {
+            if($col['data_type'] == 'boolean' OR $col['data_type'] == 'tinyint(1)')
+            {
+               $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name']." = FALSE;\n";
+            }
+            else if($col['data_type'] == 'date')
+            {
+               $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name']." = date('d-m-Y');\n";
+            }
+            else if($col['data_type'] == 'double')
+            {
+               $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name']." = 0;\n";
+            }
+            else
+            {
+               $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name']." = NULL;\n";
+            }
+         }
+         
+         $this->modelo .= $tab.$tab."}\n"
+                 . $tab."}\n\n"
+                 . $tab."public function install()\n"
+                 . $tab."{\n"
+                 . $tab.$tab."return '';\n"
+                 . $tab."}\n\n"
+                 . $tab."public function exists()\n"
+                 . $tab."{\n";
+         
+         $encontrada = FALSE;
+         foreach($this->columnas as $col)
+         {
+            if($col['column_name'] == $this->primary_key)
+            {
+               $this->modelo .= $tab.$tab.'if( is_null($this->'.$col['column_name'].") )\n"
+                       . $tab.$tab."{\n"
+                       . $tab.$tab.$tab."return FALSE;\n"
+                       . $tab.$tab."}\n"
+                       . $tab.$tab."else\n"
+                       . $tab.$tab."{\n"
+                       . $tab.$tab.$tab.'return $this->db->select('."'SELECT * FROM ".$this->tabla
+                       . " WHERE ".$col['column_name']." = '".'.$this->var2str($this->'.$col['column_name'].').'."';');\n"
+                       . $tab.$tab."}\n";
+               $encontrada = TRUE;
+               break;
+            }
+         }
+         
+         if(!$encontrada)
+         {
+            $this->modelo .= $tab.$tab."/// tu código aquí\n";
+         }
+         
+         $this->modelo .= $tab."}\n\n"
+                 . $tab."public function save()\n"
+                 . $tab."{\n"
+                 . $tab.$tab.'if( $this->exists() )'."\n"
+                 . $tab.$tab."{\n"
+                 . $tab.$tab.$tab."/// UPDATE ".$this->tabla." SET ... WHERE ...;\n"
+                 . $tab.$tab."}\n"
+                 . $tab.$tab."else\n"
+                 . $tab.$tab."{\n"
+                 . $tab.$tab.$tab."/// INSERT INTO ".$this->tabla." (...) VALUES (...);\n"
+                 . $tab.$tab."}\n"
+                 . $tab."}\n\n"
+                 . $tab."public function delete()\n"
+                 . $tab."{\n";
+         
+         $encontrada = FALSE;
+         foreach($this->columnas as $col)
+         {
+            if($col['column_name'] == $this->primary_key)
+            {
+               $this->modelo .= $tab.$tab.'return $this->db->exec('."'DELETE FROM ".$this->tabla
+                       . " WHERE ".$col['column_name']." = '".'.$this->var2str($this->'.$col['column_name'].').'."';');\n";
+               $encontrada = TRUE;
+               break;
+            }
+         }
+         
+         if(!$encontrada)
+         {
+            $this->modelo .= $tab.$tab."/// tu código aquí\n";
+         }
+         
+         $this->modelo .= $tab."}\n\n";
       }
       
-      $this->modelo .= "\n"
-              . $tab.'public function __construct($d=FALSE)'."\n"
-              . $tab."{\n"
-              . $tab.$tab."parent::__construct('".$this->tabla."');\n"
-              . $tab.$tab.'if($d)'."\n"
-              . $tab.$tab."{\n";
-      
-      foreach($this->columnas as $col)
-      {
-         if($col['data_type'] == 'boolean' OR $col['data_type'] == 'tinyint(1)')
-         {
-            $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name'].' = $this->str2bool($d'."['".$col['column_name']."']);\n";
-         }
-         else if($col['data_type'] == 'date')
-         {
-            $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name'].' = date("d-m-Y", strtotime($d'."['".$col['column_name']."']));\n";
-         }
-         else if($col['data_type'] == 'double')
-         {
-            $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name'].' = floatval($d'."['".$col['column_name']."']);\n";
-         }
-         else
-         {
-            $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name'].' = $d'."['".$col['column_name']."'];\n";
-         }
-      }
-      
-      $this->modelo .= $tab.$tab."}\n"
-              . $tab.$tab."else\n"
-              . $tab.$tab."{\n"
-              . $tab.$tab.$tab."/// valores predeterminados\n";
-      
-      foreach($this->columnas as $col)
-      {
-         if($col['data_type'] == 'boolean' OR $col['data_type'] == 'tinyint(1)')
-         {
-            $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name']." = FALSE;\n";
-         }
-         else if($col['data_type'] == 'date')
-         {
-            $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name']." = date('d-m-Y');\n";
-         }
-         else if($col['data_type'] == 'double')
-         {
-            $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name']." = 0;\n";
-         }
-         else
-         {
-            $this->modelo .= $tab.$tab.$tab.'$this->'.$col['column_name']." = NULL;\n";
-         }
-      }
-      
-      $this->modelo .= $tab.$tab."}\n"
-              . $tab."}\n\n"
-              . $tab."public function install()\n"
-              . $tab."{\n"
-              . $tab.$tab."return '';\n"
-              . $tab."}\n\n"
-              . $tab."public function exists()\n"
-              . $tab."{\n";
-      
-      $encontrada = FALSE;
-      foreach($this->columnas as $col)
-      {
-         if($col['key'] == 'PRI')
-         {
-            $this->modelo .= $tab.$tab.$tab.'if( is_null($this->'.$col['column_name'].") )\n"
-                    . $tab.$tab.$tab."{\n"
-                    . $tab.$tab.$tab.$tab."return FALSE;\n"
-                    . $tab.$tab.$tab."}\n"
-                    . $tab.$tab.$tab."else\n"
-                    . $tab.$tab.$tab."{\n"
-                    . $tab.$tab.$tab.$tab.'return $this->db->select('."'SELECT * FROM ".$this->tabla
-                    . " WHERE ".$col['column_name']." = '".'.$this->var2str($this->'.$col['column_name'].').'."';');\n"
-                    . $tab.$tab.$tab."}\n";
-            $encontrada = TRUE;
-            break;
-         }
-      }
-      
-      if(!$encontrada)
-      {
-         $this->modelo .= $tab.$tab.$tab."/// tu código aquí\n";
-      }
-      
-      $this->modelo .= $tab."}\n\n"
-              . $tab."public function save()\n"
-              . $tab."{\n"
-              . $tab.$tab.'if( $this->exists() )'."\n"
-              . $tab.$tab."{\n"
-              . $tab.$tab.$tab."/// UPDATE ".$this->tabla." SET ... WHERE ...;\n"
-              . $tab.$tab."}\n"
-              . $tab.$tab."else\n"
-              . $tab.$tab."{\n"
-              . $tab.$tab.$tab."/// INSERT INTO ".$this->tabla." (...) VALUES (...);\n"
-              . $tab.$tab."}\n"
-              . $tab."}\n\n"
-              . $tab."public function delete()\n"
-              . $tab."{\n";
-      
-      $encontrada = FALSE;
-      foreach($this->columnas as $col)
-      {
-         if($col['key'] == 'PRI')
-         {
-            $this->modelo .= $tab.$tab.$tab.'return $this->db->exec('."'DELETE FROM ".$this->tabla
-                    . " WHERE ".$col['column_name']." = '".'.$this->var2str($this->'.$col['column_name'].').'."';');\n";
-            $encontrada = TRUE;
-            break;
-         }
-      }
-      
-      if(!$encontrada)
-      {
-         $this->modelo .= $tab.$tab.$tab."/// tu código aquí\n";
-      }
-      
-      $this->modelo .= $tab."}\n\n";
       $this->modelo .= "}\n";
    }
 }
