e5ee46b50091fd826b7e8f36d595b7b8ec6d5faa
Jose Antonio Cuello <jcuello@artextrading.com>
2017-05-09 14:31:59 +0200

    Versión con Modelo base y tratamiento de Constraints
    Versión revisada, añadiendo una nueva “capa” a los modelos. Se ha
    incorporado la lectura de las Constraint para la generación del código
    del modelo de una manera más completa.

--- controller/fsdk_home.php
+++ controller/fsdk_home.php
@@ -18,11 +18,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once __DIR__.'/../lib/generar_datos_prueba.php';
+require_once __DIR__ . '/../lib/generar_datos_prueba.php';
 
-if( !defined('FS_SERVICIOS') )
-{
-   define('FS_SERVICIOS', 'servicios');
+if (!defined('FS_SERVICIOS')) {
+    define('FS_SERVICIOS', 'servicios');
 }
 
 /**
@@ -30,281 +29,220 @@ if( !defined('FS_SERVICIOS') )
  *
  * @author carlos
  */
-class fsdk_home extends fs_controller
-{
-   public $tablas;
-   public $url_recarga;
-   
-   public function __construct()
-   {
-      parent::__construct(__CLASS__, 'FSDK', 'admin');
-   }
-   
-   protected function private_core()
-   {
-      $this->url_recarga = FALSE;
-      
-      if( isset($_GET['gdp']) )
-      {
-         $gdp = new generar_datos_prueba($this->db, $this->empresa);
-         switch($_GET['gdp']) {
-            case 'fabricantes':
-               if( class_exists('fabricante') )
-               {
-                  $num = $gdp->fabricantes();
-                  $this->new_message('Generados '.$num.' fabricantes.');
-               }
-               else
-               {
-                  $this->new_error_msg('Instala el plugin facturacion_base.');
-               }
-               break;
-            
-            case 'familias':
-               if( class_exists('familia') )
-               {
-                  $num = $gdp->familias();
-                  $this->new_message('Generados '.$num.' familias.');
-               }
-               else
-               {
-                  $this->new_error_msg('Instala el plugin facturacion_base.');
-               }
-               break;
-            
-            case 'articulos':
-               if( class_exists('articulo') )
-               {
-                  $num = $gdp->articulos();
-                  $this->new_message('Generados '.$num.' artículos.');
-                  $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
-                  
-                  $this->url_recarga = $this->url().'&gdp=articulos';
-               }
-               else
-               {
-                  $this->new_error_msg('Instala el plugin facturacion_base.');
-               }
-               break;
-            
-            case 'gruposcli':
-               if( class_exists('cliente') )
-               {
-                  $num = $gdp->grupos_clientes();
-                  $this->new_message('Generados '.$num.' grupos de clientes.');
-               }
-               break;
-            
-            case 'clientes':
-               if( class_exists('cliente') )
-               {
-                  $num = $gdp->clientes();
-                  $this->new_message('Generados '.$num.' clientes.');
-                  $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
-                  
-                  $this->url_recarga = $this->url().'&gdp=clientes';
-               }
-               else
-               {
-                  $this->new_error_msg('Instala el plugin facturacion_base.');
-               }
-               break;
-            
-            case 'agentes':
-               $num = $gdp->agentes();
-               $this->new_message('Generados '.$num.' empleados.');
-               break;
-            
-            case 'proveedores':
-               if( class_exists('proveedor') )
-               {
-                  $num = $gdp->proveedores();
-                  $this->new_message('Generados '.$num.' proveedores.');
-                  $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
-                  
-                  $this->url_recarga = $this->url().'&gdp=proveedores';
-               }
-               else
-               {
-                  $this->new_error_msg('Instala el plugin facturacion_base.');
-               }
-               break;
-            
-            case 'presupuestoscli':
-               if( class_exists('presupuesto_cliente') )
-               {
-                  $num = $gdp->presupuestoscli();
-                  $this->new_message('Generados '.$num.' '.FS_PRESUPUESTOS.' de venta.');
-                  $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
-                  
-                  $this->url_recarga = $this->url().'&gdp=presupuestoscli';
-               }
-               else
-               {
-                  $this->new_error_msg('Instala el plugin presupuestos_y_pedidos.');
-               }
-               break;
-            
-            case 'pedidosprov':
-               if( class_exists('pedido_proveedor') )
-               {
-                  $num = $gdp->pedidosprov();
-                  $this->new_message('Generados '.$num.' '.FS_PEDIDOS.' de compra.');
-                  $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
-                  
-                  $this->url_recarga = $this->url().'&gdp=pedidosprov';
-               }
-               else
-               {
-                  $this->new_error_msg('Instala el plugin presupuestos_y_pedidos.');
-               }
-               break;
-            
-            case 'pedidoscli':
-               if( class_exists('pedido_cliente') )
-               {
-                  $num = $gdp->pedidoscli();
-                  $this->new_message('Generados '.$num.' '.FS_PEDIDOS.' de venta.');
-                  $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
-                  
-                  $this->url_recarga = $this->url().'&gdp=pedidoscli';
-               }
-               else
-               {
-                  $this->new_error_msg('Instala el plugin presupuestos_y_pedidos.');
-               }
-               break;
-            
-            case 'albaranesprov':
-               if( class_exists('albaran_proveedor') )
-               {
-                  $num = $gdp->albaranesprov();
-                  $this->new_message('Generados '.$num.' '.FS_ALBARANES.' de compra.');
-                  $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
-                  
-                  $this->url_recarga = $this->url().'&gdp=albaranesprov';
-               }
-               else
-               {
-                  $this->new_error_msg('Instala el plugin facturacion_base.');
-               }
-               break;
-            
-            case 'albaranescli':
-               if( class_exists('albaran_cliente') )
-               {
-                  $num = $gdp->albaranescli();
-                  $this->new_message('Generados '.$num.' '.FS_ALBARANES.' de venta.');
-                  $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
-                  
-                  $this->url_recarga = $this->url().'&gdp=albaranescli';
-               }
-               else
-               {
-                  $this->new_error_msg('Instala el plugin facturacion_base.');
-               }
-               break;
-               
-            case 'servicioscli':
-               if( class_exists('servicio_cliente') )
-               {
-                  $num = $gdp->servicioscli();
-                  $this->new_message('Generados '.$num.' '.FS_SERVICIOS.' a clientes.');
-                  $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
-                  
-                  $this->url_recarga = $this->url().'&gdp=servicioscli';
-               }
-               else
-               {
-                  $this->new_error_msg('Instala el plugin servicios.');
-               }
-               break;
-         }
-      }
-      else if( isset($_POST['generarplugin']) )
-      {
-      	if( !is_writable("plugins") )
-         {
-            $this->new_message('No tienes permisos de escritura en la carpeta plugins.');
-         }
-      	else if( $this->generar_plugin($_POST['nombre'], $_POST['descripcion']) )
-      	{
-            $this->new_message("Se generó el plugin ".$_POST['nombre']." en el directorio plugins."
-                    . " Puedes activarlo desde el <a href='index.php?page=admin_home#plugins'>panel de control</a>.");
-      	}
-      	else
-      	{
-            $this->new_error_msg("Hubo un problema al generar el plugin ".$_POST['nombre'].": Revise los logs de su servidor web.");
-      	}
-      }
-      
-      $this->tablas = $this->db->list_tables();
-   }
-   
-   private function generar_plugin($nombre, $descripcion)
-   {
-      if( !$this->crea_estructura($nombre) )
-      {
-         return FALSE;
-      }
-      else if( !$this->genera_ficheros($nombre, $descripcion) )
-      {
-         return FALSE;
-      }
-      else
-         return TRUE;
-   }
-   
-   private function crea_estructura($nombre)
-   {
-      $ok = FALSE;
-      
-      // creamos el dir del plugin
-   	if( mkdir("plugins/".$nombre) )
-   	{
-         $ok = TRUE;
-         
-         // creamos los directorios
-   		$dirs = array("controller", "view", "model");
-   		foreach($dirs as $dir)
-   		{
-            if( !mkdir("plugins/".$nombre."/".$dir) )
-            {
-               $ok = FALSE;
+class fsdk_home extends fs_controller {
+
+    public $tablas;
+    public $url_recarga;
+
+    public function __construct() {
+        parent::__construct(__CLASS__, 'FSDK', 'admin');
+    }
+
+    protected function private_core() {
+        $this->url_recarga = FALSE;
+
+        if (isset($_GET['gdp'])) {
+            $gdp = new generar_datos_prueba($this->db, $this->empresa);
+            switch ($_GET['gdp']) {
+                case 'fabricantes':
+                    if (class_exists('fabricante')) {
+                        $num = $gdp->fabricantes();
+                        $this->new_message('Generados ' . $num . ' fabricantes.');
+                    } else {
+                        $this->new_error_msg('Instala el plugin facturacion_base.');
+                    }
+                    break;
+
+                case 'familias':
+                    if (class_exists('familia')) {
+                        $num = $gdp->familias();
+                        $this->new_message('Generados ' . $num . ' familias.');
+                    } else {
+                        $this->new_error_msg('Instala el plugin facturacion_base.');
+                    }
+                    break;
+
+                case 'articulos':
+                    if (class_exists('articulo')) {
+                        $num = $gdp->articulos();
+                        $this->new_message('Generados ' . $num . ' artículos.');
+                        $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
+
+                        $this->url_recarga = $this->url() . '&gdp=articulos';
+                    } else {
+                        $this->new_error_msg('Instala el plugin facturacion_base.');
+                    }
+                    break;
+
+                case 'gruposcli':
+                    if (class_exists('cliente')) {
+                        $num = $gdp->grupos_clientes();
+                        $this->new_message('Generados ' . $num . ' grupos de clientes.');
+                    }
+                    break;
+
+                case 'clientes':
+                    if (class_exists('cliente')) {
+                        $num = $gdp->clientes();
+                        $this->new_message('Generados ' . $num . ' clientes.');
+                        $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
+
+                        $this->url_recarga = $this->url() . '&gdp=clientes';
+                    } else {
+                        $this->new_error_msg('Instala el plugin facturacion_base.');
+                    }
+                    break;
+
+                case 'agentes':
+                    $num = $gdp->agentes();
+                    $this->new_message('Generados ' . $num . ' empleados.');
+                    break;
+
+                case 'proveedores':
+                    if (class_exists('proveedor')) {
+                        $num = $gdp->proveedores();
+                        $this->new_message('Generados ' . $num . ' proveedores.');
+                        $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
+
+                        $this->url_recarga = $this->url() . '&gdp=proveedores';
+                    } else {
+                        $this->new_error_msg('Instala el plugin facturacion_base.');
+                    }
+                    break;
+
+                case 'presupuestoscli':
+                    if (class_exists('presupuesto_cliente')) {
+                        $num = $gdp->presupuestoscli();
+                        $this->new_message('Generados ' . $num . ' ' . FS_PRESUPUESTOS . ' de venta.');
+                        $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
+
+                        $this->url_recarga = $this->url() . '&gdp=presupuestoscli';
+                    } else {
+                        $this->new_error_msg('Instala el plugin presupuestos_y_pedidos.');
+                    }
+                    break;
+
+                case 'pedidosprov':
+                    if (class_exists('pedido_proveedor')) {
+                        $num = $gdp->pedidosprov();
+                        $this->new_message('Generados ' . $num . ' ' . FS_PEDIDOS . ' de compra.');
+                        $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
+
+                        $this->url_recarga = $this->url() . '&gdp=pedidosprov';
+                    } else {
+                        $this->new_error_msg('Instala el plugin presupuestos_y_pedidos.');
+                    }
+                    break;
+
+                case 'pedidoscli':
+                    if (class_exists('pedido_cliente')) {
+                        $num = $gdp->pedidoscli();
+                        $this->new_message('Generados ' . $num . ' ' . FS_PEDIDOS . ' de venta.');
+                        $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
+
+                        $this->url_recarga = $this->url() . '&gdp=pedidoscli';
+                    } else {
+                        $this->new_error_msg('Instala el plugin presupuestos_y_pedidos.');
+                    }
+                    break;
+
+                case 'albaranesprov':
+                    if (class_exists('albaran_proveedor')) {
+                        $num = $gdp->albaranesprov();
+                        $this->new_message('Generados ' . $num . ' ' . FS_ALBARANES . ' de compra.');
+                        $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
+
+                        $this->url_recarga = $this->url() . '&gdp=albaranesprov';
+                    } else {
+                        $this->new_error_msg('Instala el plugin facturacion_base.');
+                    }
+                    break;
+
+                case 'albaranescli':
+                    if (class_exists('albaran_cliente')) {
+                        $num = $gdp->albaranescli();
+                        $this->new_message('Generados ' . $num . ' ' . FS_ALBARANES . ' de venta.');
+                        $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
+
+                        $this->url_recarga = $this->url() . '&gdp=albaranescli';
+                    } else {
+                        $this->new_error_msg('Instala el plugin facturacion_base.');
+                    }
+                    break;
+
+                case 'servicioscli':
+                    if (class_exists('servicio_cliente')) {
+                        $num = $gdp->servicioscli();
+                        $this->new_message('Generados ' . $num . ' ' . FS_SERVICIOS . ' a clientes.');
+                        $this->new_message('Recargando... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
+
+                        $this->url_recarga = $this->url() . '&gdp=servicioscli';
+                    } else {
+                        $this->new_error_msg('Instala el plugin servicios.');
+                    }
+                    break;
             }
-   		}
-   	}
-      
-      return $ok;
-   }
-   
-   private function genera_ficheros($nombre, $descripcion)
-   {
-      $descripcion .= "\n<br/>Accesible desde Admin &gt; ".$nombre;
-      
-      if( !file_put_contents("plugins/".$nombre."/description", $descripcion) )
-      {
-         return FALSE;
-      }
-      if( !file_put_contents("plugins/".$nombre."/facturascripts.ini", "version = 1") )
-      {
-         return FALSE;
-      }
-      else
-      {
-         $textcontroller = str_replace( "holamundo", $nombre, file_get_contents(__DIR__.'/../tmpls/controller.php') );
-         $textvista = file_get_contents(__DIR__.'/../tmpls/view.html');
-         
-         if( !file_put_contents("plugins/".$nombre."/controller/".$nombre.".php", $textcontroller) )
-         {
-   			return FALSE;
-         }
-         else if( !file_put_contents("plugins/".$nombre."/view/".$nombre.".html", $textvista) )
-         {
-   			return FALSE;
-         }
-         else
+        } else if (isset($_POST['generarplugin'])) {
+            if (!is_writable("plugins")) {
+                $this->new_message('No tienes permisos de escritura en la carpeta plugins.');
+            } else if ($this->generar_plugin($_POST['nombre'], $_POST['descripcion'])) {
+                $this->new_message("Se generó el plugin " . $_POST['nombre'] . " en el directorio plugins."
+                   . " Puedes activarlo desde el <a href='index.php?page=admin_home#plugins'>panel de control</a>.");
+            } else {
+                $this->new_error_msg("Hubo un problema al generar el plugin " . $_POST['nombre'] . ": Revise los logs de su servidor web.");
+            }
+        }
+
+        $this->tablas = $this->db->list_tables();
+    }
+
+    private function generar_plugin($nombre, $descripcion) {
+        if (!$this->crea_estructura($nombre)) {
+            return FALSE;
+        } else if (!$this->genera_ficheros($nombre, $descripcion)) {
+            return FALSE;
+        } else
             return TRUE;
-      }
-   }
+    }
+
+    private function crea_estructura($nombre) {
+        $ok = FALSE;
+
+        // creamos el dir del plugin
+        if (mkdir("plugins/" . $nombre)) {
+            $ok = TRUE;
+
+            // creamos los directorios
+            $dirs = array("controller", "view", "model");
+            foreach ($dirs as $dir) {
+                if (!mkdir("plugins/" . $nombre . "/" . $dir)) {
+                    $ok = FALSE;
+                }
+            }
+        }
+
+        return $ok;
+    }
+
+    private function genera_ficheros($nombre, $descripcion) {
+        $descripcion .= "\n<br/>Accesible desde Admin &gt; " . $nombre;
+
+        if (!file_put_contents("plugins/" . $nombre . "/description", $descripcion)) {
+            return FALSE;
+        }
+        if (!file_put_contents("plugins/" . $nombre . "/facturascripts.ini", "version = 1")) {
+            return FALSE;
+        } else {
+            $textcontroller = str_replace("holamundo", $nombre, file_get_contents(__DIR__ . '/../tmpls/controller.php'));
+            $textvista = file_get_contents(__DIR__ . '/../tmpls/view.html');
+
+            if (!file_put_contents("plugins/" . $nombre . "/controller/" . $nombre . ".php", $textcontroller)) {
+                return FALSE;
+            } else if (!file_put_contents("plugins/" . $nombre . "/view/" . $nombre . ".html", $textvista)) {
+                return FALSE;
+            } else
+                return TRUE;
+        }
+    }
+
 }
---
+++ lib/xml_from_table.php
@@ -0,0 +1,157 @@
+<?php
+
+/*
+ * This file is part of FacturaScripts
+ * Copyright (C) 2016-2017  Carlos Garcia Gomez  neorazorx@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class xml_from_table {
+
+    private $database;
+    private $table;
+    private $xml;
+
+    /* ---------------
+     *  P R I V A T E
+     * --------------- */
+
+    private function xml_header() {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+           . "<!--\n"
+           . "    Document   : " . $this->table . ".xml\n"
+           . "    Description:\n"
+           . "        Estructura de la tabla " . $this->table . ".\n"
+           . "-->\n"
+           . "<tabla>\n"
+           . "</tabla>\n";
+    }
+
+    private function add_field($node, $column) {
+        if (isset($column['character_maximum_length']))
+            $node->addChild('tipo', $column['data_type'] . '(' . $column['character_maximum_length'] . ')');
+        else
+            $node->addChild('tipo', $column['data_type']);
+
+        if ($column['is_nullable'] == 'YES')
+            $node->addChild('nulo', 'YES');
+        else
+            $node->addChild('nulo', 'NO');
+
+        if (isset($column['column_default']))
+            $node->addChild('defecto', $column['column_default']);
+    }
+
+    private function add_serial_field($node, $column) {
+        $node->addChild('tipo', 'serial');
+        $node->addChild('nulo', 'NO');
+        $node->addChild('defecto', "nextval('" . $this->table . '_' . $column['column_name'] . "_seq'::regclass)");
+    }
+
+    private function add_constrain($name, $value) {
+        $node = $this->xml->addChild('restriccion');
+        $node->addChild('nombre', $name);
+        $node->addChild('consulta', $value);
+    }
+
+    /* -------------
+     *  P U B L I C
+     * ------------- */
+
+    public function __construct($database, $table) {
+        $this->database = $database;
+        $this->table = $table;
+        $this->xml = simplexml_load_string($this->xml_header());
+    }
+
+    public function add_columns() {
+        $columns = $this->database->get_columns($this->table);
+        foreach ($columns as $column) {
+            $node = $this->xml->addChild('columna');
+            $node->addChild('nombre', $column['column_name']);
+
+            /// comprobamos si es auto_increment
+            $auto_increment = (isset($column['extra'])
+               AND ( $column['extra'] === 'auto_increment'));
+
+            if ($auto_increment) {
+                $this->add_serial_field($node, $column);
+                continue;
+            }
+
+            /// comprobamos si es tipo serial
+            if ($column['data_type'] == 'integer' AND $column['column_default'] == "nextval('" . $this->table . '_' . $column['column_name'] . "_seq'::regclass)") {
+                $this->add_serial_field($node, $column);
+                continue;
+            }
+
+            // añadimos campo estandar
+            $this->add_field($node, $column);
+        }
+    }
+
+    public function add_constrains() {
+        $constrains = $this->database->get_constraints($this->table);
+        $primary_fields = [];
+
+        foreach ($constrains as $column) {
+            switch ($column['tipo']) {
+                case 'PRIMARY KEY':
+                case 'p': {
+                    array_push($primary_fields, $column['column_name']);
+                    break;
+                }
+
+                case 'FOREIGN KEY':
+                case 'f': {
+                    $fk = 'FOREIGN KEY (' . $column['column_name'] . ')'
+                       . ' REFERENCES ' . $column['foreign_table_name'] . '(' . $column['foreign_column_name'] . ')'
+                       . ' ON DELETE ' . $column['on_delete']
+                       . ' ON UPDATE ' . $column['on_update'];
+                    $this->add_constrain($column['restriccion'], $fk);
+                    break;
+                }
+
+                case 'u': {
+                    $this->add_constrain($column['restriccion'], 'UNIQUE (...)');
+                    break;
+                }
+
+                default: {
+                    $this->add_constrain($column['restriccion'], '...');
+                    break;
+                }
+            }
+        }
+
+        // Add Primary Keys
+        if (!empty($primary_fields))
+            $this->add_constrain(
+               $this->table . '_pkey', 'PRIMARY KEY (' . implode(",", $primary_fields) . ')'
+            );
+    }
+
+    public function read() {
+        // Formateamos XML para "Human-Readable"
+        $doc = new DOMDocument();
+        $doc->preserveWhiteSpace = false;
+        $doc->formatOutput = true;
+        $doc->loadXML($this->xml->asXML());
+
+        // devolvemos el contenido del XML
+        return $doc->saveXML();
+    }
+
+}
